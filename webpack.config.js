f4fff0c2c548dece6f10190588d9567942e4e18c
Yumenoshima Eden/占拠マンSKT豆腐屋 <skt.tofuya@gmail.com>
2020-03-21 01:24:08 +0900

    fixed webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\(.xml$/,
+                test: /\.xml$/,
                 use: [
                     'xml-loader',
                 ],
